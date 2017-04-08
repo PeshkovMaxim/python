1be42f04d49165750613db4039432031c900398b
server1 <server1@server1>
2017-04-08 10:43:40 +0300

    first commint

---
+++ tcp_server.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 1234))
+s.listen(10)
+while Tue:
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        if data == 'close': conn.close()
+        conn.send(data)
+    conn.close()
